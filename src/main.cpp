69c6dc8e1038b6b99ff3d6f77480d9cb28dd14f4
sfxfs <2501683578@qq.com>
2022-10-15 21:45:43 +0800

    增加任务句柄

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <vfd_show.h>
 #include <wifi_ntp.h>
 
+TaskHandle_t VFDShowTaskHandle = NULL;
+TaskHandle_t WiFiNTPTaskHandle = NULL;
+
 void setup()
 {
-  xTaskCreatePinnedToCore(VFDShowTask, "show the time", 1024 * 6, NULL, 1, NULL, 1);
-  xTaskCreatePinnedToCore(WiFiNTPTask, "get the time", 1024 * 6, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(VFDShowTask, "show the time", 1024 * 6, NULL, 1, &VFDShowTaskHandle, 1);
+  xTaskCreatePinnedToCore(WiFiNTPTask, "get the time", 1024 * 6, NULL, 1, &WiFiNTPTaskHandle, 1);
   vTaskDelete(NULL);
 }
 
