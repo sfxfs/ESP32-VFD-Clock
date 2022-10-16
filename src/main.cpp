ff9d19ce74efff81953f37196d9fe019e11dc455
sfxfs <2501683578@qq.com>
2022-10-16 15:28:44 +0800

    修改为准确的栈区空间分配

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,10 @@
 #include <vfd_show.h>
 #include <wifi_ntp.h>
 
-TaskHandle_t VFDShowTaskHandle = NULL;
-TaskHandle_t WiFiNTPTaskHandle = NULL;
-
 void setup()
 {
-  Serial.begin(9600);
-  xTaskCreatePinnedToCore(VFDShowTask, "show the time", 1024 * 6, NULL, 1, &VFDShowTaskHandle, 1);
-  xTaskCreatePinnedToCore(WiFiNTPTask, "get the time", 1024 * 6, NULL, 1, &WiFiNTPTaskHandle, 1);
-  Serial.printf("VFD set to: %d\n", 2 * (1024 * 6 - uxTaskGetStackHighWaterMark(VFDShowTaskHandle)));
-  Serial.printf("WiFi set to: %d", 2 * (1024 * 6 - uxTaskGetStackHighWaterMark(WiFiNTPTaskHandle)));
+  xTaskCreatePinnedToCore(VFDShowTask, "show the time", 1024 * 2, nullptr, 1, nullptr, 1);  //该任务实测占用1020字节的ram
+  xTaskCreatePinnedToCore(WiFiNTPTask, "get the time", 1024 * 4, nullptr, 1, nullptr, 1);   //占用2018字节的ram
   vTaskDelete(NULL);
 }
 
